4bd1cf6cb896a8c0fad20db75d99f15e6d3aa48a
Lingyuan Zeng <chriszeng199@163.com>
2022-06-24 20:45:15 +0800

    Initial Commit

---
+++ Shared/Demo_VersionControl_PackageManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_VersionControl_PackageManagementApp.swift
+//  Shared
+//
+//  Created by Lingyuan Zeng on 2022/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_VersionControl_PackageManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
